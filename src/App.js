b5acf34e6e1ffe5bb67b7aea000825f77a59e38e
jrsppf <46720358+jrsppf@users.noreply.github.com>
2023-05-31 12:27:07 -0400

    added ai images, added counter styling, fixed mobile breakpoints

--- src/App.js
+++ src/App.js
@@ -6,8 +6,33 @@ import fish3 from "./assets/tinned-fish-3.jpeg";
 import fish4 from "./assets/tinned-fish-4.jpeg";
 import fish5 from "./assets/tinned-fish-5.jpeg";
 import fish6 from "./assets/tinned-fish-6.jpeg";
+import fish7 from "./assets/tinned-fish-7.jpeg";
+import fish8 from "./assets/tinned-fish-8.jpeg";
+import fish9 from "./assets/tinned-fish-9.jpeg";
+import fish10 from "./assets/tinned-fish-10.jpeg";
+import fish11 from "./assets/tinned-fish-11.jpeg";
+import fish12 from "./assets/tinned-fish-12.jpeg";
+import fish13 from "./assets/tinned-fish-13.jpeg";
+import fish14 from "./assets/tinned-fish-14.jpeg";
+import fish15 from "./assets/tinned-fish-15.jpeg";
 
-const images = [fish1, fish2, fish3, fish4, fish5, fish6];
+const images = [
+  fish1,
+  fish2,
+  fish3,
+  fish4,
+  fish5,
+  fish6,
+  fish7,
+  fish8,
+  fish9,
+  fish10,
+  fish11,
+  fish12,
+  fish13,
+  fish14,
+  fish15,
+];
 
 const Loading = ({ calculatedWidth }) => (
   <aside>
@@ -21,9 +46,21 @@ const Loading = ({ calculatedWidth }) => (
 const App = () => {
   const [currentImage, setCurrentImage] = useState(0);
   const [numLoaded, setNumLoaded] = useState(0);
+  const length = images.length - 1;
 
   const handleClick = () => {
-    const length = images.length - 1;
+    setCurrentImage((currentImage) => {
+      return currentImage < length ? currentImage + 1 : 0;
+    });
+  };
+
+  const handlePrevPic = () => {
+    setCurrentImage((currentImage) => {
+      return currentImage > 0 ? currentImage - 1 : length;
+    });
+  };
+
+  const handleNextPic = () => {
     setCurrentImage((currentImage) => {
       return currentImage < length ? currentImage + 1 : 0;
     });
@@ -36,16 +73,19 @@ const App = () => {
   return (
     <section>
       <header>
-        <h1>Fishy</h1>
+        <h1>fishy</h1>
         <h2>
-          A tribute to tinned fish packaging <br />
-          by Jacob Schreiber
+          an ai image generated tribute to tinned fish<br />
+          by jacob schreiber
         </h2>
       </header>
       <figure>
         {numLoaded < images.length && (
           <Loading calculatedWidth={(numLoaded / images.length) * 100} />
         )}
+        <div className="prev" onClick={handlePrevPic}></div>
+        <div className="next" onClick={handleNextPic}></div>
+
         <figcaption>
           {currentImage + 1} / {images.length}
         </figcaption>
@@ -54,7 +94,6 @@ const App = () => {
             key={imageUrl}
             src={imageUrl}
             alt=""
-            onClick={handleClick}
             onLoad={handleImageLoad}
             className={currentImage === index ? "display" : "hide"}
           />
